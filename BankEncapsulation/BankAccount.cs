80c824b607aa660bfa5adfc4dbe793210d3d0af8
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-01 16:13:27 -0400

    Created a banking app!

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+        private double SSN;
+
+        public string Name { get; set; }
+        
+
+
+        public double Deposit(double deposit)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            return _balance += deposit;
+            
+        }
+        public double GetBalance() 
+        {
+            return _balance;
+        }
+        
+        public double Withdrawal(double withdrawal)
+        {
+            return _balance -= withdrawal;
+        }
+
+        
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            var newUser = new BankAccount();
+            Console.WriteLine("Please enter your name here:");
+            newUser.Name = Console.ReadLine();
+            var myAccountBalance = newUser.GetBalance();
+            while (true)
+            {
+                Console.WriteLine("What would you like to do today: 1. Balance Look Up 2. Deposit 3. Withdraw 4. Exit Account");
+                int userSelection;
+                if (!int.TryParse(Console.ReadLine(), out userSelection))
+
+                {
+                    Console.WriteLine("Invalid entry.\nLet's try this again...");
+                    continue;
+                }
+
+                switch (userSelection)
+                {
+                    case 1:
+                        Console.WriteLine($"Welcome, {newUser.Name} your current account balance is sitting at: {myAccountBalance}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Please, {newUser.Name}, enter your amount you would like deposited:");
+                        double deposit;
+                        if (!double.TryParse(Console.ReadLine(), out deposit))
+                        {
+                            Console.WriteLine("Invalid entry.");
+                            continue;
+                        }
+
+                        newUser.Deposit(deposit);
+                        Console.WriteLine($"Your current account balance is sitting at: {myAccountBalance}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"How Much would you like to withdraw from your account, {newUser.Name}:");
+                        double myWithdrawal;
+                        if (!double.TryParse(Console.ReadLine(), out myWithdrawal))
+                        {
+                            Console.WriteLine("Invalid entry.");
+                            continue;
+                        }
+                        newUser.Withdrawal(myWithdrawal);
+                        myAccountBalance = newUser.GetBalance();
+                        Console.WriteLine($"Your new account balance is sitting at:{myAccountBalance}");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for banking with us!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid entry.");
+                        break;
+
+                }
+            }
         }
     }
 }
